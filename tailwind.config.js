04c1ea802054f75dec186e25ef44be0bd65f7a1c
valwebd <77312053+valwebd@users.noreply.github.com>
2021-10-03 15:59:55 -0700

    tailwind-color-pallet

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [
     './components/**/*.{vue,js}',
@@ -8,7 +10,23 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+      primary: '#f7b226ff',
+      'primary-dark': '#624104ff',
+      secondary: '#537d8dff',
+      'secondary-dark': '#a5c0caff',
+      tertiary: '#a97d7eff',
+      'tertiary-dark': '#805b5fff',
+      'accent': '#971024ff',
+    },
   },
   variants: {
     extend: {},
