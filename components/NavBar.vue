435263b610cb8d70aeae00e1b54b4e42658fc7ee
OmPr366 <71120474+OmPr366@users.noreply.github.com>
2021-10-04 03:47:10 +0530

    Updated

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="row">
     <div class="col-2"><nuxt-link to="/">Home</nuxt-link></div>
-    <div class="col-2"><nuxt-link to="/">Projects</nuxt-link></div>
+    <div class="col-2"><nuxt-link to="/project">Projects</nuxt-link></div>
     <div class="col-2">
       <nuxt-link to="/coding_org">Coding Organizations</nuxt-link>
     </div>
