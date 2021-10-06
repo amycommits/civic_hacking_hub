3630871cd970700afc507f40b79e273794c0db01
Andrew Lavrentev <andrew.lavrentev@gmail.com>
2021-10-06 09:16:07 +0300

    code review fixes

--- server/db/migrations/20211005224300_code_project_nonprofit.js
+++ server/db/migrations/20211005224300_code_project_nonprofit.js
@@ -1,5 +1,5 @@
 exports.up = function (knex) {
-  return knex.schema.createTable('code_project_nonprofit', function (table) {
+  return knex.schema.createTable('code_project_nonprofits', function (table) {
     table.integer('code_organization_id').unsigned()
     table.integer('nonprofit_organization_id').unsigned()
     table.integer('project_id').unsigned()
@@ -7,14 +7,18 @@ exports.up = function (knex) {
     table.timestamp('created_at').defaultTo(knex.fn.now())
     table.timestamp('updated_at')
 
-    table.foreign('code_organization_id').references('code_organizations.id')
+    table
+      .foreign('code_organization_id')
+      .references('code_organizations.id')
     table
       .foreign('nonprofit_organization_id')
       .references('nonprofit_organizations.id')
-    table.foreign('project_id').references('projects.id')
+    table
+      .foreign('project_id')
+      .references('projects.id')
   })
 }
 
 exports.down = function (knex) {
-  return knex.schema.dropTableIfExists('code_project_nonprofit')
+  return knex.schema.dropTableIfExists('code_project_nonprofits')
 }
